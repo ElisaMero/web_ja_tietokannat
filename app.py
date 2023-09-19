0e9058d2607ab2fd710643af005c0a7d82313097
Elisa Mero <elisa.mero@helsinki.fi>
2023-09-19 21:35:39 +0300

    added pages

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from flask import Flask
-from flask import render_template
+from flask import redirect, render_template, request, session
+from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/register.html", methods=["POST"])
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+#    hash_value = generate_password_hash(password)
+ #   sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+  #  db.session.execute(sql, {"username":username, "password":hash_value})
+   # db.session.commit()
+
+
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # check username and password here!
+    session["username"] = username
+    return redirect("/")
